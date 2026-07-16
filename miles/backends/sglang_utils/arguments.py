38ddb25b2d65f7a48804cf9a1e7e21033d10e098
yueming-yuan <yym022502@gmail.com>
2026-07-16 16:35:46 -0700

    Skip agentic manual-policy default under experimental rollout refactor

--- miles/backends/sglang_utils/arguments.py
+++ miles/backends/sglang_utils/arguments.py
@@ -152,7 +152,9 @@ def validate_args(args):
     if args.sglang_dp_size > 1:
         assert args.sglang_enable_dp_attention
 
-    if args.sglang_router_policy is None and args.use_session_server:
+    from miles.utils.environ import enable_experimental_rollout_refactor
+
+    if args.sglang_router_policy is None and args.use_session_server and not enable_experimental_rollout_refactor():
         args.sglang_router_policy = "manual"
         if args.router_assignment_mode == "random":
             args.router_assignment_mode = "min_load"
