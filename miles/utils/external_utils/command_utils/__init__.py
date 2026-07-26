5c8cf2e9975961112cee0cb902858c730a7d862a
fzyzcjy <ch271828n@outlook.com>
2026-07-26 22:42:34 +0800

    Declare the command_utils re-exports with __all__ instead of a discard tuple

--- miles/utils/external_utils/command_utils/__init__.py
+++ miles/utils/external_utils/command_utils/__init__.py
@@ -18,26 +18,32 @@ from miles.utils.external_utils.command_utils.ray_backend import RayCommandBacke
 from miles.utils.external_utils.exec_command import exec_command_cpu, exec_command_gpu, exec_command_multi_node
 from miles.utils.typer_utils import dataclass_cli
 
-_ = (
-    BaseCommandBackend,
-    ExecuteTrainConfig,
-    GENERATION_HARDWARE,
-    NUM_GPUS_OF_HARDWARE,
-    build_runtime_env_vars,
-    check_has_nvlink,
-    create_run_id,
-    dataclass_cli,
-    exec_command_cpu,
-    exec_command_gpu,
-    exec_command_multi_node,
-    get_bool_env_var,
-    get_default_wandb_args,
-    get_env_enable_infinite_run,
-    parse_extra_env_vars,
-    pythonpath_with_sources,
-    repo_base_dir,
-    save_to_temp_file,
-)
+__all__ = [
+    "BaseCommandBackend",
+    "ExecuteTrainConfig",
+    "GENERATION_HARDWARE",
+    "NUM_GPUS_OF_HARDWARE",
+    "RayCommandBackend",
+    "build_runtime_env_vars",
+    "check_has_nvlink",
+    "convert_checkpoint",
+    "create_run_id",
+    "dataclass_cli",
+    "exec_command_cpu",
+    "exec_command_gpu",
+    "exec_command_multi_node",
+    "execute_train",
+    "fp8_cast_bf16",
+    "get_bool_env_var",
+    "get_default_wandb_args",
+    "get_env_enable_infinite_run",
+    "hf_download_dataset",
+    "parse_extra_env_vars",
+    "pythonpath_with_sources",
+    "repo_base_dir",
+    "rsync_simple",
+    "save_to_temp_file",
+]
 
 backend = RayCommandBackend()
 
