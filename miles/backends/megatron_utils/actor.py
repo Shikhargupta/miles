6bc9b408603407ee8358d8a50e6e0688faa48897
fzyzcjy <ch271828n@outlook.com>
2026-08-03 08:34:52 +0800

    impl

--- miles/backends/megatron_utils/actor.py
+++ miles/backends/megatron_utils/actor.py
@@ -653,13 +653,14 @@ class MegatronTrainRayActor(TrainRayActor):
         if self.args.offload_train:
             reload_process_groups()
 
-        if has_new_engines or not self.weight_updater.is_rollout_engines_fresh():
+        if has_new_engines or self.weight_updater.conn_status.needs_reconnect():
             self.weight_updater.connect_rollout_engines(
                 rollout_engines,
                 rollout_engine_lock,
                 engine_gpu_counts=engine_gpu_counts,
                 engine_gpu_offsets=engine_gpu_offsets,
             )
+            self.weight_updater.conn_status.mark_reconnected()
             dist.barrier(group=get_gloo_group())
 
         if self.args.debug_skip_weight_update:
@@ -793,4 +794,4 @@ class MegatronTrainRayActor(TrainRayActor):
             megatron_rank=dist.get_rank(),
             megatron_world_size=dist.get_world_size(),
         )
-        self.weight_updater.mark_engine_connection_stale()
+        self.weight_updater.conn_status.mark_trainer_stale()
